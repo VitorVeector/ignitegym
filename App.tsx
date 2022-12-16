a14c6f5057e5b56b5cf8a3df849b46057434d0de
Vitor Hugo <job_vitoraraujo@outlook.com>
2022-12-16 13:06:26 -0300

    Definindo Rotas

--- App.tsx
+++ App.tsx
@@ -5,8 +5,7 @@ import { NativeBaseProvider } from 'native-base';
 
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
-import { SignUp } from '@screens/SignUp';
+import { Routes } from './src/routes'
 
 export default function App() {
   const [fontIsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -18,7 +17,7 @@ export default function App() {
         barStyle="light-content"
         backgroundColor="transparent"
         translucent />
-      {fontIsLoaded ? <SignUp /> : <Loading />}
+      {fontIsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,23 @@
+import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack";
+import React from "react";
+
+import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
+
+type AuthProps = {
+    signIn: undefined,
+    signUp: undefined
+}
+
+export type AuthNavigationRoutesProps = NativeStackNavigationProp<AuthProps>
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthProps>();
+
+export function AuthRoutes() {
+    return (
+        <Navigator>
+            <Screen name="signIn" component={SignIn} />
+            <Screen name="signUp" component={SignUp} />
+        </Navigator>
+    )
+}
