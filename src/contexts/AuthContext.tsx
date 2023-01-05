4bda2030c1475aa804ec58e2e7e84bf03935900f
Vitor Hugo <job_vitoraraujo@outlook.com>
2023-01-05 12:20:02 -0300

    Definindo estado para User

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,18 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { AuthContextProps, AuthContextProviderProps } from "src/Interfaces/types";
 
 export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps)
 
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+    const [user, setUser] = useState({
+        id: "1",
+        name: "Joaquim",
+        email: "joaquim@email.com",
+        avatar: "Joaquin.png"
+    })
+
     return (
-        <AuthContext.Provider value={{
-            user: {
-                id: "",
-                name: "",
-                email: "",
-                avatar: ""
-            }
-        }}>
+        <AuthContext.Provider value={{ user }}>
             {children}
         </AuthContext.Provider>
     )
