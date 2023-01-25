3046c239ebc37071ce3c7e2a3bb19783215f913d
Vitor Hugo <vitor.silva@usebob.com.br>
2023-01-25 10:44:26 -0300

    :heavy_plus_sign: feat: Requisição de Group da API

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,25 +1,56 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
-import { HStack, VStack, FlatList, Heading, Text } from "native-base"
+import { HStack, VStack, FlatList, Heading, Text, Toast, useToast } from "native-base"
 
 import { HomeHeader } from "@components/HomeHeader"
 import { Group } from "@components/Group"
 import { ExerciseCard } from "@components/ExerciseCard"
 import { useNavigation } from "@react-navigation/native"
 import { AppRouteNavigationRoutesProps } from "@routes/app.routes"
+import { api } from "@services/api"
+import { AppError } from "@utils/AppError"
 
 
 export const Home = () => {
 
     const navigation = useNavigation<AppRouteNavigationRoutesProps>()
 
-    const [group, setGroup] = useState(["peitoral", "costas", "bíceps", "tríceps", "ombro", "abdomem", "pernas"])
+    const [group, setGroup] = useState([])
+
     const [groupSelected, setGroupSelected] = useState<string>('peitoral');
+
     const [exercices, setExercices] = useState(["Puxada", "Remada"])
 
-    function handleOpenExercice() {
+    const handleOpenExerciceDetails = () => {
         navigation.navigate("exercise")
     }
+
+    const toast = useToast()
+
+    const fetchGroups = async () => {
+        try {
+            const groups = await api.get("/groups")
+            setGroup(groups.data)
+        } catch (error) {
+            const appError = error instanceof AppError
+            const title = appError ? error.message : "Erro interno do servidor, tente novamente mais tarde"
+            toast.show({ title, placement: "top", color: "red.500" })
+        }
+    }
+
+    // const fetchExercises = async () => {
+    //     try {
+    //         const exercises = await api.get(`/exercises/bygroup/${groupSelected}`)
+    //         console.log(exercises.data)
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+    // }
+
+
+    useEffect(() => {
+        fetchGroups()
+    }, [])
 
     return (
         <VStack flex={1}>
@@ -72,7 +103,7 @@ export const Home = () => {
                     _contentContainerStyle={{ pb: 16 }}
                     showsVerticalScrollIndicator={false}
                     renderItem={({ item }) => (
-                        <ExerciseCard onPress={handleOpenExercice} />
+                        <ExerciseCard onPress={handleOpenExerciceDetails} />
                     )} />
 
             </VStack>
