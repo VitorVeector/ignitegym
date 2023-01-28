bf81cd7e9483e8b294e21a9e93c6456ed4b7b960
Vitor Hugo <vitor.silva@usebob.com.br>
2023-01-28 16:40:41 -0300

    :wrench: chore: Ajustando carregamento de imagens

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -12,12 +12,15 @@ import { useNavigation, useRoute } from "@react-navigation/native"
 import { api } from "@services/api"
 import { AppError } from "@utils/AppError"
 import { ExerciseDTO } from "@dtos/ExerciseDTO"
+import { Loading } from "@components/Loading"
 
 type RoutesParams = {
     exerciseId: string
 }
 
 export const Exercise = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
     const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO)
 
     const navigation = useNavigation<AppRouteNavigationRoutesProps>();
@@ -28,12 +31,15 @@ export const Exercise = () => {
 
     const fetchExerciseDetails = async () => {
         try {
+            setIsLoading(true)
             const response = await api.get(`/exercises/${exerciseId}`)
             setExercise(response.data)
         } catch (err) {
             const isAppError = err instanceof AppError
             const title = isAppError ? err.message : "Não foi possível carregar os exercícios."
             throw title
+        } finally {
+            setIsLoading(false)
         }
     }
 
@@ -41,54 +47,55 @@ export const Exercise = () => {
         fetchExerciseDetails()
     }, [exerciseId])
 
-    console.log(exercise)
-
     return (
         <VStack>
-            <ExerciseHeader title={exercise.name} group={exercise.group}/>
-            <ScrollView>
-                <VStack p={8} mb={16}>
-                    <Image
-                        w="full"
-                        h={80}
-                        source={{ uri: `${api.defaults.baseURL}/exercise/demo/${exercise.demo}` }}
-                        alt={exercise.name}
-                        mb={3}
-                        resizeMode="cover"
-                        rounded="lg"
-                    />
-                    <Center
-                        bg="gray.600"
-                        px={4}
-                        pt={5}
-                        pb={4}
-                        rounded="md">
-                        <HStack
-                            width="full"
-                            justifyContent="space-around">
+            <ExerciseHeader title={exercise.name} group={exercise.group} />
+
+            {isLoading ? <Loading /> :
+                <ScrollView>
+                    <VStack p={8} mb={16}>
+                        <Image
+                            w="full"
+                            h={80}
+                            source={{ uri: `${api.defaults.baseURL}/exercise/demo/${exercise.demo}` }}
+                            alt={exercise.name}
+                            mb={3}
+                            resizeMode="cover"
+                            rounded="lg"
+                        />
+                        <Center
+                            bg="gray.600"
+                            px={4}
+                            pt={5}
+                            pb={4}
+                            rounded="md">
                             <HStack
-                                alignItems="center">
-                                <Series width={24} />
-                                <Text
-                                    color="gray.200"
-                                    fontSize="lg"
-                                    ml={2}>{exercise.series} séries</Text>
+                                width="full"
+                                justifyContent="space-around">
+                                <HStack
+                                    alignItems="center">
+                                    <Series width={24} />
+                                    <Text
+                                        color="gray.200"
+                                        fontSize="lg"
+                                        ml={2}>{exercise.series} séries</Text>
+                                </HStack>
+                                <HStack
+                                    alignItems="center">
+                                    <Repetitions width={24} />
+                                    <Text
+                                        color="gray.200"
+                                        fontSize="lg"
+                                        ml={2}>{exercise.repetitions} repetições</Text>
+                                </HStack>
                             </HStack>
-                            <HStack
-                                alignItems="center">
-                                <Repetitions width={24} />
-                                <Text
-                                    color="gray.200"
-                                    fontSize="lg"
-                                    ml={2}>{exercise.repetitions} repetições</Text>
-                            </HStack>
-                        </HStack>
-                        <Button
-                            mt={6}
-                            value="Marcar como realizado" />
-                    </Center>
-                </VStack>
-            </ScrollView>
+                            <Button
+                                mt={6}
+                                value="Marcar como realizado" />
+                        </Center>
+                    </VStack>
+                </ScrollView>
+            }
 
         </VStack>
     )
